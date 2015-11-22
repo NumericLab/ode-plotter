3ab0dfcfd6a48bbb49a60f0d69d05439d12dcd79
Evgeniy Kosjakov <42_@mail.ru>
2015-11-22 21:43:47 +0300

    Removed unused usings

--- ODEPlotter/Model/IntegrationType.cs
+++ ODEPlotter/Model/IntegrationType.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ODEPlotter.Model
+﻿namespace ODEPlotter.Model
 {
-    class IntegrationType
+    public class IntegrationType
     {
 
     }
--- ODEPlotter/ViewModel/ViewModelBase.cs
+++ ODEPlotter/ViewModel/ViewModelBase.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ODEPlotter.ViewModel
+﻿namespace ODEPlotter.ViewModel
 {
-    class ViewModelBase
+    public class ViewModelBase
     {
     }
 }
