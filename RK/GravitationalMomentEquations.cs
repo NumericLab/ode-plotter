3ec716f14eff5430b63f250714c93bdfc070ea09
Evgeniy Kosjakov <42_@mail.ru>
2016-02-01 20:57:40 +0300

    RK refactoring

---
+++ RK/GravitationalMomentEquations.cs
@@ -0,0 +1,22 @@
+﻿namespace RK
+{
+    public class GravitationalMomentEquations : RungeKuttaBase
+    {
+        public GravitationalMomentEquations(int aN) : base(aN) { }
+
+        public override void F(double t, double[] y, ref double[] fy)
+        {
+            var epsilon = 0.7;
+            var delta = 1.5;
+            double p;
+            p = (y[0] * y[0] + y[1] * y[1] + y[2] * y[2] + y[3] * y[3] - 1);
+            fy[0] = -0.5 * y[1] * y[4] - 0.5 * y[2] * y[5] - 0.5 * y[3] * y[6] + 0.5 * y[2] - 0.5 * y[0] * p;
+            fy[1] = 0.5 * y[0] * y[4] + 0.5 * y[2] * y[6] - 0.5 * y[3] * y[5] - 0.5 * y[3] - 0.5 * y[1] * p;
+            fy[2] = 0.5 * y[0] * y[5] + 0.5 * y[3] * y[4] - 0.5 * y[1] * y[6] - 0.5 * y[0] - 0.5 * y[2] * p;
+            fy[3] = 0.5 * y[0] * y[6] + 0.5 * y[1] * y[5] - 0.5 * y[2] * y[4] + 0.5 * y[1] - 0.5 * y[3] * p;
+            fy[4] = (epsilon - delta) * (-y[5] * y[6] + 3 * (2 * y[2] * y[3] + 2 * y[0] * y[1]) * (y[0] * y[0] - y[1] * y[1] - y[2] * y[2] + y[3] * y[3]));
+            fy[5] = (1 - epsilon) / delta * (-y[6] * y[4] + 3 * (2 * y[1] * y[3] - 2 * y[0] * y[2]) * (y[0] * y[0] - y[1] * y[1] - y[2] * y[2] + y[3] * y[3]));
+            fy[6] = (delta - 1) / epsilon * (-y[4] * y[5] + 3 * (2 * y[1] * y[3] - 2 * y[0] * y[2]) * (2 * y[2] * y[3] + 2 * y[0] * y[1]));
+        }
+    }
+}
---
+++ RK/RungeKuttaBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace RK
+{
+    public class RungeKuttaBase
+    {
+        public int N;
+        double _t; // текущее время 
+        public double[] Y; // искомое решение Y[0] - само решение, Y[i] - i-тая производная решения
+
+        readonly double[] _yy; 
+        double[] _y1, _y2, _y3, _y4;
+
+        public RungeKuttaBase(int aN) // aN - размерность системы 
+        {
+            N = aN; // сохранить размерность системы
+
+            if (N < 1)
+            {
+                N = -1; // если размерность меньше единицы, то установить флаг ошибки
+                return; // и выйти из конструктора
+            }
+
+            Y = new double[N]; // создать вектор решения
+            _yy = new double[N]; // и внутренних решений
+            _y1 = new double[N];
+            _y2 = new double[N];
+            _y3 = new double[N];
+            _y4 = new double[N];
+        }
+
+        public void SetInit(double t0, double[] y0) // установить начальные условия.
+        {                                           // t0 - начальное время, Y0 - начальное условие
+            _t = t0;
+            int i;
+            for (i = 0; i < N; i++)
+            {
+                Y[i] = y0[i];
+            }
+        }
+
+        public double GetCurrent() // вернуть текущее время
+        {
+            return _t;
+        }
+
+        public virtual void F(double t, double[] y, ref double[] fy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextStep(double dt) // следующий шаг метода Рунге-Кутта, dt - шаг по времени (может быть переменным)
+        {
+            if (dt < 0)
+            {
+                return;
+            }
+
+            int i;
+
+            F(_t, Y, ref _y1); // расчитать Y1
+
+            for (i = 0; i < N; i++)
+            {
+                _yy[i] = Y[i] + _y1[i] * (dt / 2.0);
+            }
+            F(_t + dt / 2.0, _yy, ref _y2); // расчитать Y2
+
+            for (i = 0; i < N; i++)
+            {
+                _yy[i] = Y[i] + _y2[i] * (dt / 2.0);
+            }
+            F(_t + dt / 2.0, _yy, ref _y3); // расчитать Y3
+
+            for (i = 0; i < N; i++)
+            {
+                _yy[i] = Y[i] + _y3[i] * dt;
+            }
+            F(_t + dt, _yy, ref _y4); // расчитать Y4
+
+            for (i = 0; i < N; i++)
+            {
+                Y[i] = Y[i] + dt / 6.0 * (_y1[i] + 2.0 * _y2[i] + 2.0 * _y3[i] + _y4[i]); // расчитать решение на новом шаге
+            }
+
+            _t = _t + dt; // увеличить шаг
+
+        }
+    }
+}
