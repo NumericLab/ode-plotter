579c6f41f285848d2be1080539329a70a9bb44d2
Evgeniy Kosjakov <42_@mail.ru>
2016-02-22 23:01:55 +0300

    Solve method edited

--- RK/Solver.cs
+++ RK/Solver.cs
@@ -6,18 +6,15 @@ namespace RK
 {
     public static class Solver
     {
-        static void Solve(double tInit, double[] yInit, 
+        static void SolveOnePoint(double tInit, double[] yInit, 
             double epsilon, double delta, 
             double timePeriod, double timeStep)
         {
-            var rk4 = new GravitationalMomentEquations(0.7, 1.5);
-
-            double[] y0 = { 1.1, 0.2, 0.1, 0.3, 0.1, 1.2, 0.4 }; // зададим начальные условия y(0)=0, y'(0)=1
-
-            rk4.SetInit(0, y0);
+            var rk4 = new GravitationalMomentEquations(epsilon, delta);
+            rk4.SetInit(tInit, yInit);
             var write = new StreamWriter("Test.txt");
 
-            while (rk4.GetTime() < 10) // решаем до 10
+            while (rk4.GetTime() < timePeriod)
             {
                 write.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}", 
                     Convert.ToString(rk4.GetTime(), CultureInfo.InvariantCulture), 
@@ -29,14 +26,10 @@ namespace RK
                     Convert.ToString(rk4.Y[5], CultureInfo.InvariantCulture), 
                     Convert.ToString(rk4.Y[6], CultureInfo.InvariantCulture));
 
-                //Console.WriteLine("{0}\t{1}\t{2}", RK4.GetCurrent(), RK4.Y[0], RK4.Y[1]); // вывести t, y, y'
-
-                rk4.NextStep(0.01); // расчитать на следующем шаге, шаг интегрирования dt=0.01
+                rk4.NextStep(timeStep);
             }
-
-
+            
             write.Close();
-            //Thread.Sleep(5000);
         }
     }
 }
